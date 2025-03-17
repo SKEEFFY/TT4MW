e6a9e2721d1fb7d63f9516e9a7ea5f0c789f2665
SKEEFFY <97361822+SKEEFFY@users.noreply.github.com>
2025-03-17 20:50:03 +0300

    Game & UI controls system. Create ScrollVieew ands cards for shop.

---
+++ Assets/Scripts/Gameplay/Game Signals/Signals/UIPopupState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct UIPopupState
+{
+    public bool isOpen;
+
+    public UIPopupState(bool isOpen)
+    {
+        this.isOpen = isOpen;
+    }
+}
--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,28 +1,56 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Canvas _mainMenuButtons;
     public static GameManager Instance { get; private set; }
 
+    private Dictionary<GameAction, Action> _gameActions;
+    
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            
+            SignalBus.Subscribe<GameAction>(HandleGameActions);
+            
+            _gameActions = new Dictionary<GameAction, Action>
+            {
+                {GameAction.StartGame, StartGame},
+                {GameAction.ExitGame, ExitGame}
+            };
         }
         else
         {
             Destroy(gameObject);
         }
+        
+        Instantiate(_mainMenuButtons);
     }
 
-    public void StartGame()
+    private void HandleGameActions(GameAction action)
     {
-        Debug.Log("Game started");
+        if (_gameActions == null)
+        {
+            Debug.Log("_gameAction is NULL '_gameAction(GameManager Dictionary)");
+            return;
+        }
+        if (_gameActions.TryGetValue(action, out var gameAction))
+        {
+            gameAction();
+        }
     }
+
+    private void OnDestroy()
+    {
+        SignalBus.Unsubscribe<GameAction>(HandleGameActions);
+    }
+
+    private void StartGame() => SceneManager.LoadScene("GameScene");
+    private void ExitGame() => Debug.Log("Game exited");
 }
