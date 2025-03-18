baaa1ceee205b32b3801f1946e307f976f1d6ce2
SKEEFFY <97361822+SKEEFFY@users.noreply.github.com>
2025-03-18 20:29:23 +0300

    DataManager. SaveSystem.
    Load, save, auto save data from GameDataManager

--- Assets/Scripts/Gameplay/GameManager/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager/GameManager.cs
@@ -3,33 +3,35 @@ using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public partial class GameManager : MonoBehaviour
 {
-    [SerializeField] private Canvas _mainMenuButtons;
     public static GameManager Instance { get; private set; }
+    
+    
+    [SerializeField] private Canvas _mainMenuButtons;
 
     private Dictionary<GameAction, Action> _gameActions;
-    
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            
+
             SignalBus.Subscribe<GameAction>(HandleGameActions);
-            
+
             _gameActions = new Dictionary<GameAction, Action>
             {
-                {GameAction.StartGame, StartGame},
-                {GameAction.ExitGame, ExitGame}
+                { GameAction.StartGame, StartGame },
+                { GameAction.ExitGame, ExitGame }
             };
         }
         else
         {
             Destroy(gameObject);
         }
-        
+
         Instantiate(_mainMenuButtons);
     }
 
@@ -40,6 +42,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("_gameAction is NULL '_gameAction(GameManager Dictionary)");
             return;
         }
+
         if (_gameActions.TryGetValue(action, out var gameAction))
         {
             gameAction();
@@ -51,6 +54,26 @@ public class GameManager : MonoBehaviour
         SignalBus.Unsubscribe<GameAction>(HandleGameActions);
     }
 
-    private void StartGame() => SceneManager.LoadScene("GameScene");
-    private void ExitGame() => Debug.Log("Game exited");
+    private void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        GameDataManager.Instance.LoadGame(); 
+    }
+
+    private void ExitGame()
+    {
+        GameDataManager.Instance.SaveGame(); 
+        Debug.Log("Game exited");
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+            GameDataManager.Instance.SaveGame(); 
+    }
+
+    private void OnApplicationQuit()
+    {
+        ExitGame();
+    }
 }
---
+++ Assets/Scripts/Gameplay/Models/GameData.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+    private static GameData _instance;
+
+    private int _money;
+    private int _foodStoreLevel;
+    private int _hardwareStoreLevel;
+    private int _travelStoreLevel;
+
+    public int Money
+    {
+        get => _money;
+        set => _money = value;
+    }
+
+    public int FoodStoreLevel
+    {
+        get => _foodStoreLevel;
+        set => _foodStoreLevel = value;
+    }
+
+    public int HardwareStoreLevel
+    {
+        get => _hardwareStoreLevel;
+        set => _hardwareStoreLevel = value;
+    }
+
+    public int TravelStoreLevel
+    {
+        get => _travelStoreLevel;
+        set => _travelStoreLevel = value;
+    }
+
+    private GameData()
+    {
+        _money = 500;
+        _foodStoreLevel = 0;
+        _hardwareStoreLevel = 0;
+        _travelStoreLevel = 0;
+    }
+
+    public static GameData Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameData();
+            }
+            return _instance;
+        }
+    }
+
+    public static void SetInstance(GameData loadedData)
+    {
+        _instance = loadedData;
+        Debug.Log($"{loadedData.Money} {loadedData.FoodStoreLevel} {loadedData.HardwareStoreLevel} {loadedData.TravelStoreLevel}");
+    }
+}
